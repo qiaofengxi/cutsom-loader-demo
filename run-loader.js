d6a72800ac269c59f75de6e431edbafacc471e6e
jiangzhefeng <zhefengjiang@easyops.cn>
2019-11-02 17:15:21 +0800

    feat(): loader debug

---
+++ run-loader.js
@@ -0,0 +1,16 @@
+const { runLoaders } = require("loader-runner");
+const path = require("path");
+const fs = require("fs");
+
+runLoaders({
+    resource: path.join(__dirname, "src/demo.txt"),
+    loaders: [
+        path.join(__dirname, "src/raw-loader.js")
+    ],
+    context: {
+        minimize: true
+    },
+    readResource: fs.readFile.bind(fs)
+}, (err, result) => {
+    console.log(err || result);
+})
