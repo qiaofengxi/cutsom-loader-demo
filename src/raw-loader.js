ddc4c577ed04abaf23e96bf34742b30afc1c4393
jiangzhefeng <zhefengjiang@easyops.cn>
2019-11-02 17:38:44 +0800

    feat(): async loader callback

--- src/raw-loader.js
+++ src/raw-loader.js
@@ -1,9 +1,25 @@
+const loaderUtils = require("loader-utils");
+const path = require('path');
+const fs = require("fs");
 
 module.exports = function (resource) {
+    debugger;
+    const options = loaderUtils.getOptions(this);
+    const callback = this.async();
+    console.log(options);
     const json = JSON.stringify(resource)
         .replace(/\u2028/g, "\\u2028")
-        .replace(/\u2029/g, "\\u2029")
+        .replace(/\u2029/g, "\\u2029");
 
-    return `export default ${json}`;
+    fs.readFile(path.join(__dirname, "async.txt"), "utf-8", (err, result) => {
+        if (err) {
+            callback(err, "");
+        }
+        callback(null, result);
+    });
 
-}
+    // return `export default ${json}`;
+    // throw new Error("ddd");
+    // this.callback(new Error("erro"), json);
+
+};
